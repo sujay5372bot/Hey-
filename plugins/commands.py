a959e8aa54afd5850a96b38c4830322d21967413
sujay5372bot <sujay53721@gmail.com>
2025-04-28 08:32:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -74,7 +74,7 @@ async def start(client, message):
         await dlt.delete()
         return         
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        silenxbotz=await message.reply_sticker("CAACAgEAAxkBAAENpaZnl898tVVOj-69IH89gx-8ee-CCAACWwIAAu8vQEXX2jgCrI2F-jYE")
+        silenxbotz=await message.reply_sticker("CAACAgUAAxkBAAKsC2gO74YeZja4DVlSFDRO2amJuhmDAALcEgACXCUZVVIsS4yJXDHFNgQ")
         await asyncio.sleep(5)
         await silenxbotz.delete()
         if not await db.get_chat(message.chat.id):
