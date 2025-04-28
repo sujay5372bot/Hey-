d038e9607e01c68f80efbe1a13ef55cbd33386a2
sujay5372bot <sujay53721@gmail.com>
2025-04-28 19:07:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,9 +39,9 @@ PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002157033795'))
 
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002692803410') 
+reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002424418647') 
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002692803410') 
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002424418647') 
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
 DATABASE_URI = environ.get('DATABASE_URI', "")
